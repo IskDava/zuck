bcbb24f1dea3110d31f1f5ad93c0c9c5142d43c9
Dava <davidiskieveshton@gmail.com>
2025-06-22 22:57:38 +0300

    Major update

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const option1 = document.getElementById('option1'),
-option2 = document.getElementById('option2');
+const plus = document.getElementById('plus-sign')
+plus.addEventListener('mouseover', () => {
+    plus.setAttribute("src", "plus_yellow.png")
+})
+plus.addEventListener('mouseout', () => {
+    plus.setAttribute("src", "plus_white.png")
+})
+const profile = document.getElementById('profile-sign')
+profile.addEventListener('mouseover', () => {
+    profile.setAttribute("src", "profile_yellow.png")
+})
+profile.addEventListener('mouseout', () => {
+    profile.setAttribute("src", "profile_white.png")
+})
+const home = document.getElementById('home-sign')
+home.addEventListener('mouseover', () => {
+    home.setAttribute("src", "home_yellow.png")
+})
+home.addEventListener('mouseout', () => {
+    home.setAttribute("src", "home_white.png")
+})
+
+
+let count = 0;
+let nextRound = [];
+let round = 1;
+
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
 
 const people = {
     "boys" : [
@@ -108,59 +135,79 @@ const people = {
         "Вера Шурыгина"
     ]
 }
-let count = 0
+
+const captions = document.querySelectorAll("figcaption.caption");
+captions[0].innerText = people.boys[count++];
+captions[1].innerText = people.boys[count++];
 
 function win(winner) {
     if (!winner.classList.contains("winner")) {
-    const win = document.createElement("p")
-    win.innerText = "Winner!"
-    win.classList.add("winner-title")
-    winner.append(win)
-    winner.classList.add("winner")
+        winner.classList.add("winner");
     }
-    if (! document.querySelector(".next-versus")) {
-        const btn = document.createElement("button")
-        btn.innerText = "Next versus"
-        btn.classList.add("next-versus")
-        const main = document.getElementById("main-container")
-        main.appendChild(btn)
+    
+    if (!document.querySelector(".next-versus")) {
+        const btn = document.createElement("button");
+        btn.innerText = "Next versus";
+        btn.classList.add("next-versus");
+        const main = document.getElementById("main-container");
+        main.appendChild(btn);
+
         btn.addEventListener("click", () => {
-            const winner = document.querySelector(".winner")
-            if (winner && count < people.boys.length) {
-                const captions = document.querySelectorAll("figcaption.caption")
-                for (let caption of captions) {
-                    caption.innerText = people.boys[count]
-                    count++
+            option1.classList.remove("highlighted")
+            option2.classList.remove("highlighted")
+            const winner = document.querySelector(".winner");
+            if (winner) {
+                nextRound.push(winner.querySelector("figcaption.caption").innerText);
+            }
+
+            loose(option1);
+            loose(option2);
+            btn.remove();
+
+            const captions = document.querySelectorAll("figcaption.caption");
+            console.log(nextRound)
+            if (count + 1 < people.boys.length) {
+                captions[0].innerText = people.boys[count++];
+                captions[1].innerText = people.boys[count++];
+            } else if (nextRound.length > 1) {
+                people.boys = nextRound;
+                if (nextRound.length % 2 == 1){
+                    nextRound = [nextRound[nextRound.length - 1]]
                 }
-                loose(option2)
-                loose(option1)
+                else {
+                    nextRound = [];
+                }
+                count = 0;
+                round++;
+                captions[0].innerText = people.boys[count++];
+                captions[1].innerText = people.boys[count++];
+            } else {
+                captions[0].innerText = nextRound[0];
+                captions[1].innerText = "";
+                alert("Турнир выиграл " + nextRound[0]);
             }
-            else if (count >= people.boys.length){
-                const choice = document.getElementById("choice")
-                main.removeChild(choice)
-                main.removeChild(btn)
-                const end = document.createElement("p")
-                end.innerText = "That's the End"
-                end.classList.add("end-text")
-                main.appendChild(end)
-            }
-        })
+        });
     }
 }
 
 function loose(looser) {
-    const title = looser.querySelector(".winner-title")
+    const title = looser.querySelector(".winner-title");
     if (title) {
-    looser.removeChild(title)
-    looser.classList.remove("winner")
+        looser.removeChild(title);
+        looser.classList.remove("winner");
     }
 }
 
 option1.addEventListener("click", () => {
-    win(option1)
-    loose(option2)
-})
+    win(option1);
+    option1.classList.add("highlighted")
+    loose(option2);
+    option2.classList.remove("highlighted")
+});
+
 option2.addEventListener("click", () => {
-    win(option2)
-    loose(option1)
-})
+    win(option2);
+    option2.classList.add("highlighted")
+    loose(option1);
+    option1.classList.remove("highlighted")
+});
