c1d78c014ed30870d10b762ee009e440972ff380
Dava <davidiskieveshton@gmail.com>
2026-07-13 14:09:46 -0700

    Bugfix

--- zucks/apps.py
+++ zucks/apps.py
@@ -6,6 +6,6 @@ class ZucksConfig(AppConfig):
 
     def ready(self):
         from .download_default import get_default_zuck
-        from .models import add_zuck_model, write_zuck_model
-        add_zuck_model(get_default_zuck(1))
-        add_zuck_model(get_default_zuck(2))
+        from .models import write_zuck_model, write_zuck_model
+        write_zuck_model(get_default_zuck(1))
+        write_zuck_model(get_default_zuck(2))
